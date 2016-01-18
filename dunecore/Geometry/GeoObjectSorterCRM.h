bf341d97051f99bfda9db8e5f758fb558f13fcf6
Vyacheslav Galymov <vgalymov@ipnl.in2p3.fr>
2016-01-18 04:14:45 -0600

    sorting algorithms for dual-phase geometry

---
+++ dunecore/Geometry/GeoObjectSorterCRM.h
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////
+/// \file  GeoObjectSorterCRM.h
+/// \brief Interface to algorithm class for sorting dual-phase CRM of geo::XXXGeo objects
+///
+/// \version $Id:  $
+/// \author  brebel@fnal.gov vgalymov@ipnl.in2p3.fr
+////////////////////////////////////////////////////////////////////////
+#ifndef GEO_GEOOBJECTSORTERCRM_H
+#define GEO_GEOOBJECTSORTERCRM_H
+
+#include <vector>
+
+#include "Geometry/GeoObjectSorter.h"
+
+namespace geo{
+
+  class GeoObjectSorterCRM : public GeoObjectSorter {
+
+  public:
+
+    GeoObjectSorterCRM(fhicl::ParameterSet const& p);
+    ~GeoObjectSorterCRM();
+
+    void SortAuxDets        (std::vector<geo::AuxDetGeo*>          & adgeo)    const;
+    void SortAuxDetSensitive(std::vector<geo::AuxDetSensitiveGeo*> & adsgeo)   const;
+    void SortCryostats      (std::vector<geo::CryostatGeo*>        & cgeo)     const;
+    void SortTPCs     	    (std::vector<geo::TPCGeo*>      	   & tgeo)     const;
+    void SortPlanes   	    (std::vector<geo::PlaneGeo*>    	   & pgeo,	      
+		      	     geo::DriftDirection_t     	     const & driftDir) const;
+    void SortWires    	    (std::vector<geo::WireGeo*>     	   & wgeo)     const;
+    
+  private:
+    
+  };
+
+}
+
+#endif // GEO_GEOOBJECTSORTERCRM_H
