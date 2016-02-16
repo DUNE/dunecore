3c4c9718ac97e838b24dbfd93043b9ab561f6cf9
David Adams <dladams@bnl.gov>
2016-02-16 09:24:11 -0500

    Add interface ChannelMappingService.

---
+++ dunecore/DuneInterface/ChannelMappingService.h
@@ -0,0 +1,36 @@
+// ChannelMappingService.h
+
+// David Adams
+// December 2015
+//
+// Interface for a service that maps online to offline channels.
+
+#ifndef ChannelMappingService_H
+#define ChannelMappingService_H
+
+#include <vector>
+#include <iostream>
+#include "dune/DuneInterface/AdcTypes.h"
+#include "art/Framework/Services/Registry/ServiceMacros.h"
+
+class ChannelMappingService {
+
+public:
+
+  typedef unsigned int Channel;
+
+  // Map online to offline.
+  virtual Channel offline(Channel onlineChannel) const =0;
+
+  // Map offline to online.
+  virtual Channel online(Channel offlineChannel chan) const =0;
+
+  // Print parameters.
+  virtual std::ostream& print(std::ostream& out =std::cout, std::string prefix ="") const =0;
+
+};
+
+DECLARE_ART_SERVICE_INTERFACE(ChannelMappingService, LEGACY)
+
+#endif
+
