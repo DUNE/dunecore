25015af781cc5e89a0f0c3b5c74bbefb97b4ae02
Michael Baird <mbaird42@dunegpvm07.fnal.gov>
2016-03-18 09:22:48 -0500

    Moving everything to a separate template directory - this is just cleanup and reorganization.

---
+++ dunecore/DAQTriggerSim/Service/TemplateTriggerService.h
@@ -0,0 +1,52 @@
+// TemplateTriggerService.h
+//
+// Michael Baird
+// March 2016
+//
+// Template trigger service class for the DAQ trigger service framework.
+//
+
+#ifndef TEMPLATETRIGGERSERVICE_H
+#define TEMPLATETRIGGERSERVICE_H
+
+#include "dune/DuneInterface/BaseTriggerService.h"
+
+#include <memory>
+#include <string>
+#include <iostream>
+
+namespace fhicl {
+  class ParameterSet;
+}
+namespace art {
+  class ActivityRegistry;
+}
+
+class TemplateTriggerService : public BaseTriggerService {
+  
+ public:
+  
+  // Simple constructor...
+  TemplateTriggerService();
+  
+  // Constructor from fcl parameters...
+  TemplateTriggerService(fhicl::ParameterSet const & pset, art::ActivityRegistry&);
+  
+  // Make the trigger decision.
+  //
+  //   params:
+  //   evt - The ART event, to be sliced and diced as the trigger algorithm
+  //         sees fit...
+  bool Trigger(art::Event const & evt);
+
+ private:
+  
+  // Parameters.
+  unsigned int fIndex;
+  std::string  fAString;
+  
+};
+
+DECLARE_ART_SERVICE_INTERFACE_IMPL(TemplateTriggerService, BaseTriggerService, LEGACY)
+
+#endif
---
+++ dunecore/DAQTriggerSim/Service/TemplateTriggerService_service.cc
@@ -0,0 +1,55 @@
+// TemplateTriggerService_service.cc
+//
+// Michael Baird
+// March 2016
+//
+// Template trigger service class for the DAQ trigger service framework.
+//
+
+// DUNETPC specific includes
+#include "dune/DAQTriggerSim/Service/TemplateTriggerService.h"
+
+// Framework includes
+#include "fhiclcpp/ParameterSet.h"
+
+// C++ includes
+#include <iostream>
+
+
+
+//.....................................................................
+TemplateTriggerService::TemplateTriggerService() :
+  fIndex(0),
+  fAString("no value...")
+{ }
+
+
+
+//.....................................................................
+TemplateTriggerService::TemplateTriggerService(const fhicl::ParameterSet& pset, art::ActivityRegistry&) :
+  fIndex(0),
+  fAString("no value...")
+{
+  fAString = pset.get<std::string>("AString");
+}
+
+
+
+//.....................................................................
+bool TemplateTriggerService::Trigger(art::Event const & evt)
+{
+  
+  std::cout << "\n\nIn the Trigger function for event "
+	    << evt.id().event()
+	    << "\n\nfIndex = " << fIndex
+	    << "\nString = " << fAString
+	    << "\n\n\n";
+  
+  fIndex++;
+  
+  return true;
+}
+
+
+
+DEFINE_ART_SERVICE_INTERFACE_IMPL(TemplateTriggerService, BaseTriggerService)
