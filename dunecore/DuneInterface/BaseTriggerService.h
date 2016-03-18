a3eca1d2e6c92c8a2716d6e615991fc624207aad
Michael Baird <mbaird42@dunegpvm07.fnal.gov>
2016-03-18 08:23:20 -0500

    Simplifying the required functions in the trigger base class.

--- dunecore/DuneInterface/BaseTriggerService.h
+++ dunecore/DuneInterface/BaseTriggerService.h
@@ -28,34 +28,12 @@ public:
   // Destructor:
   virtual ~BaseTriggerService() = default;
 
-
-
-  // Make a trigger decision based on TPC info only.
+  // Make your trigger decision.
   //
   //   params:
   //   evt - The ART event, to be sliced and diced as the trigger algorithm
   //         sees fit...
-  virtual bool TPCtrigger(art::Event const & evt)
-  { return false; }
-
-
-
-  // Make a trigger decision based on PD info only.
-  //
-  //   params:
-  //   evt - The ART event, to be sliced and diced as the trigger algorithm
-  //         sees fit...
-  virtual bool PDtrigger(art::Event const & evt)
-  { return false; }
-
-
-
-  // Make a trigger decision from both TPC and PD info.
-  //
-  //   params:
-  //   evt - The ART event, to be sliced and diced as the trigger algorithm
-  //         sees fit...
-  virtual bool TPCPDtrigger(art::Event const & evt)
+  virtual bool Trigger(art::Event const & evt)
   { return false; }
 
 };
