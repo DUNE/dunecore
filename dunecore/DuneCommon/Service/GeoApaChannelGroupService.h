45abb3d08ded78bc81820804d718ff5bfa2953af
David Adams <dladams@bnl.gov>
2017-10-24 10:38:12 -0400

    Add opt to skip ROPs in GeoApaChannelGroupService
    https://cdcvs.fnal.gov/redmine/issues/18004

--- dunecore/DuneCommon/Service/GeoApaChannelGroupService.h
+++ dunecore/DuneCommon/Service/GeoApaChannelGroupService.h
@@ -4,6 +4,19 @@
 // October 2016
 //
 // APA channel groups extracted from larsoft geometry.
+// Group igrp holds the channels from APA igrp.
+//
+// The array ApaRops specifies which ROPs are kept for each group.
+// If it is empty, all ROPS are retained.
+// Otherwise the bit pattern at ApaRops[igrp%nent] (nent = ApaRops.size())
+// specifies which ROPS to to keep. E.g. 15 = 0xa = b_1111 to keep all four or
+// 11 = 0xb = 1011 to keep ROPs 0, 1 and 3.
+// Note the DUNE plane ordering from highest to lowest is x2-x1-v-u so that
+// 7 keeps planes from the first TPC and 11 keeps the planes for the 2nd TPC.
+//
+// Parameters:
+//   LogLevel - 0=silent, 1=init only, 2=every call
+//   ApaRops - Bit pattern indicating which ROPs to keep (see above).
 
 #ifndef GeoApaChannelGroupService_H
 #define GeoApaChannelGroupService_H
@@ -43,6 +56,10 @@ private:
   typedef std::vector<ChannelVector> ChannelVectorVector;
  
   art::ServiceHandle<geo::Geometry> m_pgeo;
+
+  // Parameters.
+  int m_LogLevel;
+  std::vector<int> m_ApaRops;
 
   Index m_size;
   NameVector m_names;
