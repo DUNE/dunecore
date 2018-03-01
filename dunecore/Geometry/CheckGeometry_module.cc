6dfd071358ec9cf9b8fff0a859d40348753db51e
Lynn Garren <garren@fnal.gov>
2018-03-01 17:05:48 -0600

    remove override

--- dunecore/Geometry/CheckGeometry_module.cc
+++ dunecore/Geometry/CheckGeometry_module.cc
@@ -48,7 +48,7 @@ public:
   void analyze(art::Event const & e) override;
 
   // Selected optional functions.
-  void reconfigure(fhicl::ParameterSet const & p) override;
+  void reconfigure(fhicl::ParameterSet const & p) ;
 
 private:
 
