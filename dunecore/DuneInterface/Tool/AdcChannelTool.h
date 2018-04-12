331c209e63d7874403d2de6fe4b4fa72f49e9a69
David Adams <dladams@bnl.gov>
2018-04-12 13:53:48 -0400

    Pretty up AdcChannelTool.h

--- dunecore/DuneInterface/Tool/AdcChannelTool.h
+++ dunecore/DuneInterface/Tool/AdcChannelTool.h
@@ -3,7 +3,7 @@
 // David Adams
 // April 2018
 //
-// Interface for tools that view or modify single objects o
+// Interface for tools that view or modify single objects or
 // maps of ADC channel data.
 //
 // All methods return a DataMap object including a status that should be set
@@ -15,7 +15,7 @@
 // Viewing might return histograms, graphs or calculated data in its DataMap.
 // Or it might write png or root files
 //
-// All methods have a default implementation. A typcial tool implemention
+// All methods have a default implementation. A typical tool implemention
 // will override one and, where appropriate, the defaults will use that
 // ovewrride. If no methods are overriden or an inapppropriate call is made,
 // all methods return status interfaceNotImplemented();
@@ -39,40 +39,57 @@ public:
 
   // Modify data for one channel.
   // Default returns error.
-  virtual DataMap modify(AdcChannelData&) const {
-    return DataMap(interfaceNotImplemented());
-  }
+  virtual DataMap modify(AdcChannelData&) const;
 
   // View data for one channel.
-  // Default calls modify with a copy of the data.
-  virtual DataMap view(const AdcChannelData& acd) const {
-    AdcChannelData adctmp(acd);
-    return modify(adctmp);
-    //return modify(AdcChannelData(acd));
-  }
+  // Default calls modify with a copy of the data and returns the
+  // DataMap from that call.
+  virtual DataMap view(const AdcChannelData& acd) const;
 
   // Modify data for multiple channels.
-  // Default loops over channels and appends the result for each success.
-  // Returned status is the number of failures.
-  virtual DataMap modifyMap(AdcChannelDataMap& acds) const {
-    DataMap ret;
-    int nfail = 0;
-    for ( AdcChannelDataMap::value_type& iacd : acds ) {
-      DataMap dm = modify(iacd.second);
-      if ( dm.status() == interfaceNotImplemented() ) return DataMap(interfaceNotImplemented());
-      else if ( dm.status() ) ++nfail;
-      else ret += dm;
-    }
-    ret.setStatus(nfail);
-    return ret;
-  }
+  // Default calls modify for each channel and appends the result for each success.
+  // The status is set to the number of failures.
+  virtual DataMap modifyMap(AdcChannelDataMap& acds) const;
 
   // View data for multiple channels.
-  // Default loops over channels and appends the result for each success.
-  // Returned status is the number of failures.
+  // Default call view for each channel and appends the result for each success.
+  // The status is set to the number of failures.
   virtual DataMap viewMap(AdcChannelDataMap& acds) const;
 
 };
+
+//**********************************************************************
+// Definitions for the above declarations.
+//**********************************************************************
+
+inline
+DataMap AdcChannelTool::modify(AdcChannelData&) const {
+  return DataMap(interfaceNotImplemented());
+}
+
+//**********************************************************************
+
+inline
+DataMap AdcChannelTool::view(const AdcChannelData& acd) const {
+  AdcChannelData adctmp(acd);
+  return modify(adctmp);
+}
+
+//**********************************************************************
+
+inline
+DataMap AdcChannelTool::modifyMap(AdcChannelDataMap& acds) const {
+  DataMap ret;
+  int nfail = 0;
+  for ( AdcChannelDataMap::value_type& iacd : acds ) {
+    DataMap dm = modify(iacd.second);
+    if ( dm.status() == interfaceNotImplemented() ) return DataMap(interfaceNotImplemented());
+    else if ( dm.status() ) ++nfail;
+    else ret += dm;
+  }
+  ret.setStatus(nfail);
+  return ret;
+}
 
 //**********************************************************************
 
