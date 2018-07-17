2cfb0642b5e10e911fffdcde1c81aa3e1fd993a2
Jason Stock <jstock@dunegpvm08.fnal.gov>
2018-07-16 21:40:22 -0500

    Renaming Obj folder. Fixing OpDetDigitizer to fill DivRecs correctly with channel and particle information

---
+++ dunecore/DuneObj/classes.h
@@ -0,0 +1,23 @@
+// Build a dictionary.
+//
+// $Author:  $
+// $Date: 2010/04/12 18:12:28 $
+//
+// Original author Rob Kutschke, modified by klg
+
+#include "canvas/Persistency/Common/PtrVector.h"
+#include "canvas/Persistency/Common/Wrapper.h"
+#include "canvas/Persistency/Common/Assns.h"
+
+#include "dune/DuneObj/EventRecord.h"
+#include "dune/DuneObj/OpDetDivRec.h"
+#include "lardataobj/RecoBase/OpFlash.h"
+#include "lardataobj/RecoBase/Hit.h"
+#include "lardataobj/Simulation/OpDetBacktrackerRecord.h"
+#include "lardataobj/RawData/OpDetWaveform.h"
+
+#include <vector>
+#include <map>
+
+
+//template class art::wrapper<art::Assns<raw::OpDetWaveform,            sim::OpDetBacktrackerRecord,   void>  >
