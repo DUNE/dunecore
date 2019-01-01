2c01abb2549e133442622ec373f57721ef274fc6
David Adams <dladams@bnl.gov>
2019-01-01 17:22:21 -0600

    A pedestal to offsetLine.

--- dunecore/DuneCommon/offsetLine.h
+++ dunecore/DuneCommon/offsetLine.h
@@ -7,6 +7,8 @@
 // and equal-magnitude, opposite-sign x-offsets on either side of
 // the origin. The function is continuous for offset >= 0 and has
 // a discontinuity at x=0 for offset < 0.
+//
+// The offsetLinePed function adds a pedestal to offsetLIne.
 
 #ifndef offsetLine_H
 #define offsetLine_H
@@ -31,6 +33,12 @@ double offsetLine(double* px, double* ppar) {
 }
 
 inline
+double offsetLinePed(double* px, double* ppar) {
+  double yped = ppar[2];
+  return yped + offsetLine(px, ppar);
+}
+
+inline
 TF1* offsetLineTF1(double off =0.0, double slope =1.0,
                    double xmin =-10.0, double xmax =10.0,
                    std::string fname ="offsetLine") {
@@ -42,4 +50,18 @@ TF1* offsetLineTF1(double off =0.0, double slope =1.0,
   return pf;
 }
 
+inline
+TF1* offsetLinePedTF1(double off =0.0, double slope =1.0, double ped =0.0,
+                      double xmin =-10.0, double xmax =10.0,
+                      std::string fname ="offsetLinePed") {
+  TF1* pf = new TF1(fname.c_str(), offsetLinePed, xmin, xmax, 3);
+  pf->SetParName(0, "Offset");
+  pf->SetParName(1, "Slope");
+  pf->SetParName(2, "Pedestal");
+  pf->SetParameter(0, off);
+  pf->SetParameter(1, slope);
+  pf->SetParameter(2, ped);
+  return pf;
+}
+  
 #endif
