a11bb840aa8acf23b486bcfa88deefa3f77e61c4
David Adams <dladams@bnl.gov>
2019-01-07 09:51:21 -0600

    Add offsetLineFull.

--- dunecore/DuneCommon/offsetLine.h
+++ dunecore/DuneCommon/offsetLine.h
@@ -17,6 +17,26 @@
 #include "TF1.h"
 
 inline
+double offsetLineFull(double* px, double* ppar) {
+  double xoff = ppar[0];
+  double scal = ppar[1];
+  double yped = ppar[2];
+  double sneg = ppar[3];
+  double x = px[0];
+  double gain = scal;
+  if ( x < 0.0 ) gain *= sneg;
+  if ( xoff > 0.0 ) {
+    if ( x > xoff ) return yped + gain*(x-xoff);
+    if ( x < -xoff ) return yped + gain*(x+xoff);
+  } else {
+    double y0 = -gain*xoff;
+    if ( x > 0.0 ) return yped + y0 + gain*x;
+    else if ( x < 0.0 ) return yped - y0 + gain*x;
+  }
+  return 0.0;
+}
+
+inline
 double offsetLine(double* px, double* ppar) {
   double xoff = ppar[0];
   double gain = ppar[1];
@@ -61,6 +81,23 @@ TF1* offsetLinePedTF1(double off =0.0, double slope =1.0, double ped =0.0,
   pf->SetParameter(0, off);
   pf->SetParameter(1, slope);
   pf->SetParameter(2, ped);
+  return pf;
+}
+  
+inline
+TF1* offsetLineFullTF1(double off =0.0, double slope =1.0,
+                       double ped =0.0, double sneg =1.0,
+                       double xmin =-10.0, double xmax =10.0,
+                       std::string fname ="offsetLinePed") {
+  TF1* pf = new TF1(fname.c_str(), offsetLineFull, xmin, xmax, 4);
+  pf->SetParName(0, "Offset");
+  pf->SetParName(1, "Slope");
+  pf->SetParName(2, "Pedestal");
+  pf->SetParName(3, "NegScale");
+  pf->SetParameter(0, off);
+  pf->SetParameter(1, slope);
+  pf->SetParameter(2, ped);
+  pf->SetParameter(3, sneg);
   return pf;
 }
   
