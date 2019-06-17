9273499a8a3109a94653ff9cebbf6f6c70de100f
David Adams <dladams@bnl.gov>
2019-06-17 13:16:23 -0500

    Add and use bad event tool.

---
+++ dunecore/DuneCommon/Tool/FclIndexVectorMapTool_tool.cc
@@ -0,0 +1,62 @@
+// FclIndexVectorMapTool_tool.cc
+
+#include "FclIndexVectorMapTool.h"
+#include "fhiclcpp/intermediate_table.h"
+#include "fhiclcpp/make_ParameterSet.h"
+#include <iostream>
+#include <iomanip>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::setw;
+using Name = std::string;
+
+//**********************************************************************
+
+FclIndexVectorMapTool::FclIndexVectorMapTool(fhicl::ParameterSet const& ps)
+: m_LogLevel(ps.get<Index>("LogLevel")),
+  m_Entries(ps.get<IndexVectorVector>("Entries")) {
+  const Name myname = "FclIndexVectorMapTool::ctor: ";
+  for ( const IndexVector ent : m_Entries ) {
+    if ( ent.size() == 0 ) continue;
+    Index ient = ent.front();
+    IndexVector& vals = m_entryMap[ient];
+    vals.insert(vals.end(), ++ent.begin(), ent.end());
+  }
+  Index vecCount = m_entryMap.size();
+  Index valCount = 0;
+  for ( const IndexVectorMap::value_type& ent : m_entryMap ) valCount += ent.second.size();
+  if ( m_LogLevel >= 1 ) {
+    cout << myname << "  LogLevel: " << m_LogLevel << endl;
+    cout << myname << "   Entries: " << vecCount << " vectors with "
+         << valCount << " values";
+    if ( m_LogLevel > 1 ) {
+      cout << ":" << endl;
+      for ( const IndexVectorMap::value_type& ent : m_entryMap ) {
+        cout << "  " << ent.first << ": [";
+        bool first = true;
+        for ( Index val : ent.second ) {
+          if ( first ) first = false;
+          else cout << ", ";
+          cout << val;
+        }
+        cout << "]" << endl;
+      }
+    } else {
+      cout << "." << endl;
+    }
+  }
+}
+
+//**********************************************************************
+
+const IndexVectorMapTool::IndexVector& FclIndexVectorMapTool::get(Index idx) const {
+  const Name myname = "FclIndexVectorMapTool::get: ";
+  IndexVectorMap::const_iterator ient = m_entryMap.find(idx);
+  static const IndexVector empty;
+  if ( ient == m_entryMap.end() ) return empty;
+  return ient->second;
+}
+
+//**********************************************************************
---
+++ dunecore/DuneCommon/Tool/test/test_FclIndexVectorMapTool.cxx
@@ -0,0 +1,116 @@
+// test_FclIndexVectorMapTool.cxx
+//
+// David Adams
+// April 2017
+//
+// Test FclIndexVectorMapTool.
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "dune/ArtSupport/DuneToolManager.h"
+#include "dune/DuneInterface/Tool/IndexVectorMapTool.h"
+#include "TH1F.h"
+
+#undef NDEBUG
+#include <cassert>
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::ofstream;
+using std::istringstream;
+using fhicl::ParameterSet;
+using Index = unsigned int;
+using IndexVector = std::vector<Index>;
+
+//**********************************************************************
+
+int test_FclIndexVectorMapTool(bool useExistingFcl =false, Index runin =0) {
+  const string myname = "test_FclIndexVectorMapTool: ";
+#ifdef NDEBUG
+  cout << myname << "NDEBUG must be off." << endl;
+  abort();
+#endif
+  string line = "-----------------------------";
+
+  cout << myname << line << endl;
+  string fclfile = "test_FclIndexVectorMapTool.fcl";
+  if ( ! useExistingFcl ) {
+    cout << myname << "Creating top-level FCL." << endl;
+    ofstream fout(fclfile.c_str());
+    if ( runin == 0 ) {
+      fout << "tools: {" << endl;
+      fout << "  mytool: {" << endl;
+      fout << "    tool_type: FclIndexVectorMapTool" << endl;
+      fout << "    LogLevel: 3" << endl;
+      fout << "    Entries: [ [100, 2, 3], [101, 12] ]" << endl;
+      fout << "  }" << endl;
+      fout << "}" << endl;
+    }
+    fout.close();
+  } else {
+    cout << myname << "Using existing top-level FCL." << endl;
+  }
+
+  cout << myname << line << endl;
+  cout << myname << "Fetching tool manager." << endl;
+  DuneToolManager* ptm = DuneToolManager::instance(fclfile);
+  assert ( ptm != nullptr );
+  DuneToolManager& tm = *ptm;
+  tm.print();
+  assert( tm.toolNames().size() >= 1 );
+
+  cout << myname << line << endl;
+  cout << myname << "Fetching tool." << endl;
+  auto pvm = ptm->getPrivate<IndexVectorMapTool>("mytool");
+  assert( pvm != nullptr );
+
+  cout << myname << line << endl;
+  cout << "Fetch index 100." << endl;
+  const IndexVector& vec0 = pvm->get(100);
+  assert( vec0.size() == 2 );
+  const IndexVector& vec1 = pvm->get(101);
+  assert( vec1.size() == 1 );
+  const IndexVector& vec2 = pvm->get(102);
+  assert( vec2.size() == 0 );
+  
+  cout << myname << line << endl;
+  cout << "Check contains." << endl;
+  assert( pvm->contains(100, 2) );
+  assert( pvm->contains(100, 3) );
+  assert( ! pvm->contains(100, 4) );
+  assert( pvm->contains(101, 12) );
+  assert( ! pvm->contains(101, 4) );
+  assert( ! pvm->contains(102, 4) );
+
+  cout << myname << line << endl;
+  cout << myname << "Done." << endl;
+  return 0;
+}
+
+//**********************************************************************
+
+int main(int argc, char* argv[]) {
+  bool useExistingFcl = false;
+  Index run = 0;
+  if ( argc > 1 ) {
+    string sarg(argv[1]);
+    if ( sarg == "-h" ) {
+      cout << "Usage: " << argv[0] << " [keepFCL] [RUN]" << endl;
+      cout << "  If keepFCL = true, existing FCL file is used." << endl;
+      cout << "  If RUN is nonzero, the data for that run are displayed." << endl;
+      return 0;
+    }
+    useExistingFcl = sarg == "true" || sarg == "1";
+  }
+  if ( argc > 2 ) {
+    string sarg(argv[2]);
+    istringstream ssarg(argv[2]);
+    ssarg >> run;
+  }
+  return test_FclIndexVectorMapTool(useExistingFcl, run);
+}
+
+//**********************************************************************
