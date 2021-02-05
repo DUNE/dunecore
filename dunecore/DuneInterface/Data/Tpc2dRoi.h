ac202369c89518310e7f2a185b9760dcc8eaa602
David Adams <dladams@bnl.gov>
2021-02-05 08:28:20 -0600

    Make Tpc2dRoi sample offset long.

--- dunecore/DuneInterface/Data/Tpc2dRoi.h
+++ dunecore/DuneInterface/Data/Tpc2dRoi.h
@@ -18,19 +18,20 @@ class Tpc2dRoi {
 public:
 
   using Index = unsigned int;
+  using LongIndex = unsigned long int;
   using DataArray = Real2dData<float>;
   using IndexArray = DataArray::IndexArray;
 
   // Ctor for an ROI with no size.
   Tpc2dRoi() : m_sampleOffset(0), m_channelOffset(0) { }
 
-  // Ctor for an ROI with no size.
-  Tpc2dRoi(Index ncha, Index nsam, Index icha0, Index isam0 =0)
+  // Ctor for an ROI with size.
+  Tpc2dRoi(Index ncha, Index nsam, Index icha0, LongIndex isam0 =0)
   : m_data({ncha, nsam}), m_sampleOffset(isam0), m_channelOffset(icha0) { }
 
   // Return the offset for the first tick wrt to a common reference.
   // Step size is one data tick assumed the same for all channels in the plane.
-  Index sampleOffset() const { return m_sampleOffset; }
+  LongIndex sampleOffset() const { return m_sampleOffset; }
 
   // Return the number of ticks.
   Index sampleSize() const { return data().nSamples()[1]; }
@@ -48,7 +49,7 @@ public:
 private:
 
   Real2dData<float> m_data;
-  Index m_sampleOffset;
+  LongIndex m_sampleOffset;
   Index m_channelOffset;
 
 };
