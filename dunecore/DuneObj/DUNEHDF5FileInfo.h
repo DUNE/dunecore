d122132f0b4b60c40858893228d510224315a4f1
Jacob Calcutt <calcuttj@msu.edu>
2021-10-18 19:21:57 -0500

    Fleshed out the HDF input source. Have some placeholders for things like run number/subrun/event

--- dunecore/DuneObj/DUNEHDF5FileInfo.h
+++ dunecore/DuneObj/DUNEHDF5FileInfo.h
@@ -3,6 +3,12 @@
 #include <hdf5.h>
 namespace raw {
 class DUNEHDF5FileInfo {
+ public:
+  DUNEHDF5FileInfo(std::string filename, hid_t file_handle, int version,
+                   std::string group)
+   : fFileName(filename), fHDF5FileHandle(file_handle),
+     fFormatVersion(version), fEventGroupName(group) {};
+  DUNEHDF5FileInfo(std::initializer_list<DUNEHDF5FileInfo>) {};
  private:
   std::string fFileName;
   hid_t fHDF5FileHandle;
