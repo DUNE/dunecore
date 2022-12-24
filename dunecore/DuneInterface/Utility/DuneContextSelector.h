492b09e320f5cfa6d7b857e1d2844a5c85a8ed86
GCO_admin <gco_admin@fnal.gov>
2022-12-24 21:33:01 +0000

    Add DuneContextSelector and test. #48

---
+++ dunecore/DuneInterface/Utility/DuneContextSelector.h
@@ -0,0 +1,28 @@
+// DuneContextSelector.h
+//
+// Interface for a utility or tool that uses a context to select a name.
+
+#ifndef DuneContextSelector_H
+#define DuneContextSelector_H
+
+#include "dunecore/DuneInterface/Data/DuneContext.h"
+#include <string>
+
+class DuneContextSelector {
+
+public:
+
+  using Name = std::string;
+  using Context = DuneContext;
+
+  // Dtor.
+  virtual ~DuneContextSelector() {}
+
+  // Return name based on context.
+  // Pointer is passed so implementation can cast and can handle
+  // missing context (null pointer).
+  virtual Name getName(const Context* pcon) const =0;
+
+};
+
+#endif
