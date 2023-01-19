30a543762f72424ce80e31e2faf4a89d8bdda12f
GCO_admin <gco_admin@fnal.gov>
2023-01-19 14:13:00 +0000

    Remove inadvertent file. #45

--- dunecore/ChannelStatus/ToolBasedChannelStatusService_tool.cc
+++
@@ -1,6 +0,0 @@
-#include "dunecore/ChannelStatus/ToolBasedChannelStatusService.h"
-
-#include "art/Framework/Services/Registry/ServiceDefinitionMacros.h"
-
-DEFINE_ART_SERVICE_INTERFACE_IMPL(ToolBasedChannelStatusService,
-                                  lariov::ChannelStatusService)
