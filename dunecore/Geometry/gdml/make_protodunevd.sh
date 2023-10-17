d34fb4beac305fb540f24dbffcbd540b17e3be5b
Hamza Amar <98878419+Hamza-Amar@users.noreply.github.com>
2023-10-17 18:47:10 +0200

    Update make_protodunevd.sh
    Remove _nowires extension when w=1

--- dunecore/Geometry/gdml/make_protodunevd.sh
+++ dunecore/Geometry/gdml/make_protodunevd.sh
@@ -4,8 +4,8 @@ perl make_refactored_gdml.pl -i protodunevd_v4_nowires.xml -o protodunevd_v4_ref
 rm protodunevd_v*_nowires.xml
 rm protodune-vd*parts.gdml
 
-perl generate_protodunevd_v4_refactored.pl -o protodunevd_v4_nowires.xml -w=1 -s="parts"
-perl make_refactored_gdml.pl -i protodunevd_v4_nowires.xml -o protodunevd_v4_refactored.gdml
+perl generate_protodunevd_v4_refactored.pl -o protodunevd_v4.xml -w=1 -s="parts"
+perl make_refactored_gdml.pl -i protodunevd_v4.xml -o protodunevd_v4_refactored.gdml
 
 rm protodunevd_v*.xml
 rm protodune-vd*parts.gdml
