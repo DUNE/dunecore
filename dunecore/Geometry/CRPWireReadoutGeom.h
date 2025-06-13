DUNE/dunecore

commit 7be102b3102ca21f664311107a029ee3a9deffe5

Update on CRPWireReadout to update heritated functions from the default WireReadout (#147)
Co-authored-by: Jose Soto <j.soto@cern.sh>

dunecore/Geometry/CRPWireReadoutGeom.h
@@ -28,6 +28,11 @@
 #include <cassert>
 #include <utility>
 
+// ART
+#include "art/Utilities/make_tool.h"
+
+#include "duneprototypes/Protodune/vd/ChannelMap/PDMapAlg.h"
+#include "duneprototypes/Protodune/vd/ChannelMap/PDVDPDMapAlg.hh"
 
 // -----------------------------------------------------------------------------
 // forward declarations
@@ -587,6 +592,22 @@ class geo::CRPWireReadoutGeom: public geo::WireReadoutGeom {
   //
   std::string fLogCategory = "CRPWireReadoutGeom";
   
+    /// @name Optical detector channel mapping
+
+  std::unique_ptr<opdet::PDVDPDMapAlg> fPDMapTool;
+//  virtual unsigned int NOpChannels(unsigned int NOpDets) const override;
+//  unsigned int NOpChannels() const  override;
+//  unsigned int MaxOpChannel() const override;
+  bool IsValidOpChannel(unsigned int opChannel, unsigned int NOpDets) const;
+  bool IsValidOpChannel(int opChannel) const;
+  unsigned int NOpChannels(unsigned int NOpDets) const;
+  unsigned int MaxOpChannel(unsigned int NOpDets) const;
+  unsigned int NOpHardwareChannels(unsigned int opDet) const;
+  unsigned int OpChannel(unsigned int detNum, unsigned int ch) const;
+  unsigned int OpDetFromOpChannel(unsigned int opChannel) const;
+  unsigned int HardwareChannelFromOpChannel(unsigned int /* opChannel */) const;
+  unsigned int NOpChannels() const;
+
 }; // class geo::CRPWireReadoutGeom
 
 

dunecore/Geometry/CMakeLists.txt
@@ -24,9 +24,11 @@ art_make( BASENAME_ONLY
                         art::Utilities
                         canvas::canvas
                         ROOT::Geom
-                        
+                        nlohmann_json::nlohmann_json
+                        duneprototypes::Protodune_vd_ChannelMap
+                        opdet_PDVDPDMapAlg
           SERVICE_LIBRARIES larcorealg::Geometry
-                     larcore::Geometry_Geometry_service
+                        larcore::Geometry_Geometry_service
                         dunecore_Geometry
                         fhiclcpp::fhiclcpp
                         cetlib::cetlib
@@ -34,6 +36,8 @@ art_make( BASENAME_ONLY
                         ROOT::Geom
                         ROOT::XMLIO
                         ROOT::Gdml
+                        duneprototypes::Protodune_vd_ChannelMap
+                        opdet_PDVDPDMapAlg
         )
 
 cet_build_plugin ( CheckGeometry art::module

dunecore/Geometry/CRPWireReadoutGeom.cxx
@@ -37,8 +37,14 @@
 #include <functional> // std::mem_fn()
 #include <tuple>
 
+// ART
+#include "art/Utilities/make_tool.h"
+
 #include "CRPWireReadoutGeom.h"
 #include "GeoObjectSorterCRU60D.h"
+
+#include "duneprototypes/Protodune/vd/ChannelMap/PDVDPDMapAlg.hh"
+
 
 using geo::dune::vd::crp::ChannelToWireMap;
 
@@ -307,7 +313,7 @@ geo::CRPWireReadoutGeom::CRPWireReadoutGeom(fhicl::ParameterSet const& p,
   
   buildReadoutPlanes(geom->Cryostats());
   fillChannelToWireMap(geom->Cryostats());
-  
+  fPDMapTool = art::make_tool<opdet::PDVDPDMapAlg>(p.get<fhicl::ParameterSet>("PDMapTool"));
   MF_LOG_TRACE(fLogCategory)
     << "CRPWireReadoutGeom::Initialize() completed.";
 }
@@ -952,5 +958,59 @@ std::string geo::CRPWireReadoutGeom::PlaneTypeName(PlaneType_t planeType) {
   
 } // geo::CRPWireReadoutGeom::PlaneTypeName()
 
-
 // ----------------------------------------------------------------------------
+bool geo::CRPWireReadoutGeom::IsValidOpChannel(unsigned int opChannel, unsigned int NOpDets) const {
+   return fPDMapTool->isValidHardwareChannel(opChannel);
+}
+bool geo::CRPWireReadoutGeom::IsValidOpChannel(int opChannel) const {
+   return fPDMapTool->isValidHardwareChannel(opChannel);
+}
+
+//----------------------------------------------------------------------------
+unsigned int geo::CRPWireReadoutGeom::NOpChannels(unsigned int NOpDets) const
+{
+  // By default just return the number of optical channels (hardware channels)
+  return fPDMapTool->NOpChannels();
+}
+
+//----------------------------------------------------------------------------
+unsigned int geo::CRPWireReadoutGeom::MaxOpChannel(unsigned int NOpDets) const
+{
+  // By default just return the number of optical channels
+  return fPDMapTool->NOpChannels();
+}
+
+//----------------------------------------------------------------------------
+unsigned int geo::CRPWireReadoutGeom::NOpHardwareChannels(unsigned int opDet) const
+{
+  // Number of hardware channels per opDet
+  return fPDMapTool->NOpHardwareChannels(opDet);
+}
+
+//----------------------------------------------------------------------------
+unsigned int geo::CRPWireReadoutGeom::OpChannel(unsigned int detNum, unsigned int ch) const
+{
+  //What is this?
+  return ch;
+}
+
+//----------------------------------------------------------------------------
+unsigned int geo::CRPWireReadoutGeom::OpDetFromOpChannel(unsigned int opChannel) const
+{
+  return fPDMapTool->OpDetFromOpChannel(opChannel);
+}
+//----------------------------------------------------------------------------
+unsigned int geo::CRPWireReadoutGeom::HardwareChannelFromOpChannel(unsigned int opDet) const
+{
+  if(fPDMapTool->NOpHardwareChannels(opDet)==1) return fPDMapTool->HardwareChannelPerOpDet(opDet).at(0);
+  else
+  {
+     throw cet::exception(fLogCategory)
+	<< "Ambiguous request of Op Hardware channel for an OpChannel that has more than one.\n";
+  }
+}
+//......................................................................
+unsigned int geo::CRPWireReadoutGeom::NOpChannels() const
+{
+  return fPDMapTool->NOpChannels();
+}

commit fa2be15a7a8e100e8b9c1749667d671f47470c38

Fix: Add missing override specifiers to CRPWireReadoutGeom for Clang compatibility. This fixes build errors caused by -Winconsistent-missing-override when compiled with Clang 14 or newer. The issue was introduced in commit 7be102b.

dunecore/Geometry/CRPWireReadoutGeom.h
@@ -598,14 +598,14 @@ class geo::CRPWireReadoutGeom: public geo::WireReadoutGeom {
 //  virtual unsigned int NOpChannels(unsigned int NOpDets) const override;
 //  unsigned int NOpChannels() const  override;
 //  unsigned int MaxOpChannel() const override;
-  bool IsValidOpChannel(unsigned int opChannel, unsigned int NOpDets) const;
+  bool IsValidOpChannel(unsigned int opChannel, unsigned int NOpDets) const override;
   bool IsValidOpChannel(int opChannel) const;
-  unsigned int NOpChannels(unsigned int NOpDets) const;
-  unsigned int MaxOpChannel(unsigned int NOpDets) const;
-  unsigned int NOpHardwareChannels(unsigned int opDet) const;
-  unsigned int OpChannel(unsigned int detNum, unsigned int ch) const;
-  unsigned int OpDetFromOpChannel(unsigned int opChannel) const;
-  unsigned int HardwareChannelFromOpChannel(unsigned int /* opChannel */) const;
+  unsigned int NOpChannels(unsigned int NOpDets) const override;
+  unsigned int MaxOpChannel(unsigned int NOpDets) const override;
+  unsigned int NOpHardwareChannels(unsigned int opDet) const override;
+  unsigned int OpChannel(unsigned int detNum, unsigned int ch) const override;
+  unsigned int OpDetFromOpChannel(unsigned int opChannel) const override;
+  unsigned int HardwareChannelFromOpChannel(unsigned int /* opChannel */) const override;
   unsigned int NOpChannels() const;
 
 }; // class geo::CRPWireReadoutGeom