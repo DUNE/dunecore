763ed894d5fb8146df20cc0760e45c1a71143cfc
Jose Soto <j.soto@cern.sh>
2025-07-03 14:42:39 +0200

    PDHD geometry: Updated XArapuca and Field cage  positions in Y Added make script.

---
+++ dunecore/Geometry/gdml/make_protodunehd.sh
@@ -0,0 +1,32 @@
+#Run this script directly on lxplus, no container is needed, do not setup larsoft.
+
+perl -I. generate_protodunehd_v7_refactored.pl -o protodunehd_v7_nowires.xml -w=0 -s="parts" -u=0 -p=1
+perl make_gdml.pl -i protodunehd_v7_nowires.xml -o protodunehd_v7_refactored_nowires.gdml
+
+rm protodunehd_v7_nowires.xml
+rm pdhd_*parts.gdml
+
+perl -I. generate_protodunehd_v7_refactored.pl -o protodunehd_v7.xml -w=1 -s="parts" -u=0 -p=1
+perl make_gdml.pl -i protodunehd_v7.xml -o protodunehd_v7_refactored.gdml
+
+rm protodunehd_v7.xml
+rm pdhd_*parts.gdml
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
